10cc98f18bb86d25fa294164d913ac6ec2fab551
Roizor <aidenjs9@icloud.com>
2023-05-20 09:19:36 -0500

    Coolio

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 // bitwarden-dedupe-js config
 // check: Method of checking for duplications (user_pass)
 // use: Remove or warn on dupe found (rm, warn)
-// file: Create or replace file (new, replace)
 module.exports = {
     check: [
         'user_pass'
     ],
-    use: 'rm',
-    file: 'new'
+    use: 'rm'
 }
