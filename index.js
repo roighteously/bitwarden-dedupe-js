0e5837d8bf6860093e86e1025f4a294c70c3fe06
Roizor <aidenjs9@icloud.com>
2023-05-20 06:52:01 -0700

    gdsfdfg

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 // important
-const exported = 'bitwarden_export_20230520064529.json'
+const exported = './bitwarden_export_20230520064529.json';
+
+const fs = require('fs');
+const parsed = JSON.parse(fs.readFileSync(exported));
+
+parsed.items.forEach(item => {
+    console.log(item)
+})
