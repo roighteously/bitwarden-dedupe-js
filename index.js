a7c99802924d9509048632c2e397e495822a3995
Roizor <aidenjs9@icloud.com>
2023-05-20 08:59:35 -0500

    simple dupe check

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const exported = './bitwarden_export_20230520064529.json';
 const fs = require('fs');
 const parsed = JSON.parse(fs.readFileSync(exported));
 
+let lastItem = {}; // init last item object for previous checking
+
 parsed.items.forEach(item => {
-    console.log(item)
+    // console.log(item)
+    if(lastItem.name == item.name) {
+        // Website is potentially same, lets check user & pass
+        if(!lastItem.login.password == item.login.password) return; // lets check user
+        if(!lastItem.login.username == item.login.username) return; // its the sme
+        console.log('[dupe found]', item.name);
+        console.log('username:', lastItem.login.username);
+        console.log('---------')
+        return;
+    }; // most simple, if it's the exact same dont
+    console.log(item.name)
+    lastItem = item;
 })
