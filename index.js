aa3ac0a2d9238a152457f05a3410d370a027d543
Aiden <aidenjs9@icloud.com>
2023-10-23 22:12:52 -0500

    I really don't know what I fixed but I fixed it but for real this time. Now supports notes.

--- index.js
+++ index.js
@@ -87,10 +87,14 @@ const checksList = [
     {
         name: 'user_pass',
         exec: (item, lastItem, cfg) => {
-            if(lastItem.name == item.name) {
+            console.log(item)
+            if (!('login' in item) || !('login' in lastItem)) return;
+            if (!('password' in item.login)) return;
+            if (!('password' in lastItem.login)) return;
+            if(lastItem.name === item.name) {
                 // Website is potentially same, lets check user & pass
-                if (!lastItem.login.password === item.login.password) return; // lets check user
-                if (!lastItem.login.username === item.login.username) return; // its the sme
+                if (lastItem.login.password !== item.login.password) return; // lets check user
+                if (lastItem.login.username !== item.login.username) return; // its the sme
                 if (cfg.use.includes('warn')) console.log('[dupe found]', item.name, 'username:', lastItem.login.username);
                 return true;
             };
